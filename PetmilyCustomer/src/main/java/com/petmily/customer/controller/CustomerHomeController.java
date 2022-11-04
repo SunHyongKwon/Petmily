6d838fea7e5683957fda84744d10f97ef5daeae6
soonhyong <mikekwon42@gmail.com>
2022-11-04 16:36:51 +0900

    [add] signup_check / _form.jsp

--- PetmilyCustomer/src/main/java/com/petmily/customer/controller/CustomerHomeController.java
+++ PetmilyCustomer/src/main/java/com/petmily/customer/controller/CustomerHomeController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.petmily.customer.command.CustomerCommand;
 
 
-@WebServlet("*.do")
+@WebServlet("*")
 public class CustomerHomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
