f16719f5a07771bda84f7971dbcf09ba387c3cf7
kosukjin <sukjinko78@gmail.com>
2022-11-09 16:19:53 +0900

    [add admin_mypage_list]

--- PetmilyAdmin/src/main/java/com/petmily/admin/dao/AdminDAO.java
+++ PetmilyAdmin/src/main/java/com/petmily/admin/dao/AdminDAO.java
@@ -2,10 +2,14 @@ package com.petmily.admin.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.petmily.admin.dto.adminDTO;
 
 public class AdminDAO {
 
@@ -20,6 +24,45 @@ public class AdminDAO {
 			e.printStackTrace();
 		}
 	}
+	// 석진[content view]
+	public adminDTO adminContentView(String sadid) {
+		
+	adminDTO dto =null;
+	Connection connection = null;
+	PreparedStatement preparedStatement = null;
+	ResultSet resultSet = null;// 검색
+
+	try {
+		connection = dataSource.getConnection();
+
+		String query = "select *  from  petmily  where  adid = "+sadid;
+		preparedStatement = connection.prepareStatement(query);
+		resultSet = preparedStatement.executeQuery();
+
+		if (resultSet.next()) {
+			String adid = resultSet.getString("adid");
+			String adname = resultSet.getString("adname");
+			String ademail = resultSet.getString("ademail");
+			String adpw = resultSet.getString("adpw");
+			Timestamp adinitdate = resultSet.getTimestamp("adinitdate"); 
+
+			dto = new adminDTO(adid, adname, ademail, adpw, adinitdate);
+
+		}
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if (resultSet != null)resultSet.close();
+			if (preparedStatement != null)preparedStatement.close(); 
+			if (connection != null)connection.close(); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	return dto;
+}//content_view
 	
 	// gukHwa[수정]
 	public void update(String adpw, String adname, String adphone, String ademail) {
