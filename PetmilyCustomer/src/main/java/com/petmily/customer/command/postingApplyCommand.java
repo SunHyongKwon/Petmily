7eff15dd6cf34c9246ca1c8855805fb3c973b478
jinhyeong <wlsgud0452@naver.com>
2022-11-14 14:00:55 +0900

    [add,update] PostingClickCommand.java, board_view.jsp

---
+++ PetmilyCustomer/src/main/java/com/petmily/customer/command/postingApplyCommand.java
@@ -0,0 +1,22 @@
+package com.petmily.customer.command;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class postingApplyCommand implements CustomerCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int executeInt(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
