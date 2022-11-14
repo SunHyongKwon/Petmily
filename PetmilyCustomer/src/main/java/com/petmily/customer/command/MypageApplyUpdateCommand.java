9c78a7bceaf7d5591f318fa90416647ea48d944a
soonhyong <mikekwon42@gmail.com>
2022-11-14 18:30:51 +0900

    [dao] update apply list

--- PetmilyCustomer/src/main/java/com/petmily/customer/command/MypageApplyUpdateCommand.java
+++ PetmilyCustomer/src/main/java/com/petmily/customer/command/MypageApplyUpdateCommand.java
@@ -5,39 +5,37 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.petmily.customer.dao.ApplyDAO;
 import com.petmily.customer.dao.ReviewDAO;
 
 public class MypageApplyUpdateCommand implements CustomerCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String func = request.getParameter("func");
 		String index = request.getParameter("index");
 		String[] apidArr = request.getParameterValues("apid");
-		
+		String[] pidArr = request.getParameterValues("pid");
+
 		// index로 어떤 apid에서 선택 된 것인지 알아오기
 		String apid = apidArr[Integer.parseInt(index)];
+		String pid = apidArr[Integer.parseInt(index)];
 
 		String columnname = "";
-		
+
 		// 어떤 버튼 눌렀는지에 따라 업데이트 해줘야 하는 컬럼명이 다름
-		if(func.equals("accept")) {
-			columnname = "apmatchingdate";
-			// 수락일 때 
-		}else {
-			// 거절일 때
-			columnname = "apcanceldate";
-		}
-		
+		columnname = "apmatchingdate";
+		// 수락일 때
+
 		// review table에 그거 업데이트
-		ReviewDAO dao = new ReviewDAO();
-		dao.update(Integer.parseInt(apid),columnname);
+		ApplyDAO dao = new ApplyDAO();
+		dao.updateByApId(Integer.parseInt(apid), columnname);
+		dao.updateByPId(Integer.parseInt(pid), columnname);
 
 	}
 
 	@Override
 	public int executeInt(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
+
 		return 0;
 	}
 
